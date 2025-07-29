c2cf5e148490cb145b88d232163164c91e72a341
sanjoy <godwindsouza522@outlook.com>
2025-07-30 02:28:37 +0530

    Downgraded the keras version

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ test_df=pd.concat([input_data.drop(['Geography'],axis=1),one_hot_geo_df],axis=1)
 test_df=standard.transform(test_df)
 
 #load model
-model=tf.keras.models.load_model('model.keras')
+model=tf.keras.models.load_model('model.h5')
 
 predict_value=model.predict(test_df)
 
